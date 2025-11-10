81a63bbde7b0df3d5d58899a0b0dd6c78d94b0be
kyBa-Le <lekyba2000hc@gmail.com>
2025-11-10 10:33:41 +0700

    complete db reset

--- App.tsx
+++ App.tsx
@@ -1,52 +1,23 @@
-
-import { StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-} from 'react-native-safe-area-context';
-import Home from './src/page/Home';
-import { useEffect, useState } from 'react';
-import { createTables, getDbConnection, getProducts, insertSampleDb } from './src/database/dbService';
-import { Product } from './src/type/ProductType';
-import Banner from './src/component/Banner';
-import NavigationBar from './src/component/NavigationBar';
+import { Text } from "react-native";
+import { getDbConnection } from "./src/database/dbService";
+import { useEffect } from "react";
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-  const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
     const initDb = async () => {
       const db = await getDbConnection();
-      await createTables(db);
-      await insertSampleDb(db);
-      const products = await getProducts(db);
-      setProducts(products);
+      await db.executeSql('DROP TABLE IF EXISTS products;');
+      await db.executeSql('DROP TABLE IF EXISTS categories;');
     }
     initDb();
   }, []);
 
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent product={products} />
-    </SafeAreaProvider>
+    <Text>
+      KHI BẠN THẤY MÀN HÌNH NÀY TỨC LÀ TOÀN BỘ DATABASE ĐÃ ĐƯỢC RESET
+    </Text>
   );
 }
-
-function AppContent({ product }: any) {
-  return (
-    <View style={styles.container}>
-      <NavigationBar />
-      <Banner />
-      <Home products={product} />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
