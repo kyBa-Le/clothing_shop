b69559b3ac1ff1dc3892e4793e513566ddb09126
kyBa-Le <lekyba2000hc@gmail.com>
2025-11-25 21:20:50 +0700

    develop the basic user bottom tab

--- src/page/Category.tsx
+++ src/page/Category.tsx
@@ -1,4 +1,4 @@
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { FlatList, StyleSheet, Text, TouchableOpacity, View } from "react-native"
 import { CategoryType } from "../type/CategoryType";
 import { getCategories } from "../service/CategoryService";
