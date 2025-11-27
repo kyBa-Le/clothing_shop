2114a24125735ee9638eedcdb6896f895ba81e11
kyBa-Le <lekyba2000hc@gmail.com>
2025-11-27 10:38:44 +0700

    add the local storage to store login session

--- App.tsx
+++ App.tsx
@@ -3,6 +3,8 @@ import AppRoute from './src/component/StackRoute';
 import { UserType } from './src/type/UserType';
 import { useEffect, useState } from 'react';
 import { AuthContext } from './src/component/AuthContext';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { LOGGED_IN_USER_KEY } from './src/service/AuthService';
 
 export type RootStackParamList = {
   Main: undefined;
@@ -16,6 +18,16 @@ export type RootStackParamList = {
 
 function App() {
   const [user, setUser] = useState<UserType | null>(null);
+  
+  useEffect(() => {
+    const loadUser = async () => {
+      const storedUser = await AsyncStorage.getItem(LOGGED_IN_USER_KEY);
+      if (storedUser) {
+        setUser(JSON.parse(storedUser));
+      }
+    };
+    loadUser();
+  }, []);
 
   return (
     <AuthContext.Provider value={{ user, setUser }}>
--- src/service/AuthService.tsx
+++ src/service/AuthService.tsx
@@ -1,11 +1,18 @@
 import { getUser } from "./UserService";
 import { UserType } from "../type/UserType";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const login = async (username: string, password: string) => {
     const user = await getUser({username, password});
+    if (user != null) {
+        await AsyncStorage.setItem(LOGGED_IN_USER_KEY, JSON.stringify(user));
+    }
     return user;
 }
 
 export const logout = async ({setUser}: {setUser: React.Dispatch<React.SetStateAction<UserType | null>>}) => {
+    await AsyncStorage.removeItem(LOGGED_IN_USER_KEY);
     setUser(null);
 }
+
+export const LOGGED_IN_USER_KEY = "loggedInUser";
